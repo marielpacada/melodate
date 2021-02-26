275eed40462788b3f4050ac20aa55acd81481d47
Mariel Pacada <49132861+marielpacada@users.noreply.github.com>
2021-02-26 00:05:41 -0800

    getting token:

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
+global.fetch = require("node-fetch");
 const express = require("express");
 const app = express();
 
 const { my_client_id } = require('./secrets/auth.js');
+const { my_client_secret } = require('./secrets/auth.js');
+const my_client_id_64 = Buffer.from(my_client_id).toString('base64');
+const my_client_secret_64 = Buffer.from(my_client_secret).toString('base64');
 const redirect_uri = "http://localhost:3000/callback";
 
 app.set("view engine", "pug");
+app.set("query parser", "extended");
 app.use(express.static("public"));
-
-app.get("/callback", function (req, res) { 
-    res.sendFile(__dirname + "/public/pug/landing.html");
-});
-
 
 // TIME FOR HEAVYLIFTING WOO
 // get auth from spotify (watch the black guy's video, desktop 2 hehe)
 // also watch angela's course to review how to use express
 
-
 app.get('/login', function (req, res) {
-    var scopes = 'user-read-private user-read-email';
+    console.log(my_client_id);
+    console.log(my_client_secret);
+    var scopes = 'user-top-read playlist-modify-public user-follow-modify';
     res.redirect('https://accounts.spotify.com/authorize' +
         '?response_type=code' +
         '&client_id=' + my_client_id +
@@ -26,16 +27,35 @@ app.get('/login', function (req, res) {
         '&redirect_uri=' + encodeURIComponent(redirect_uri));
 });
 
+/* ---------------- NOW YOU HAVE USER AUTH, GET ALL THE DATA NEEDED!!! ---------------- */
 
-// NOW YOU HAVE USER AUTH, GET ALL THE DATA NEEDED!!!
+// getting refresh token
+app.get("/callback", function (req, res) {
+    getTokens(req, res);
+});
 
+async function getTokens(request, response) {
+    response.sendFile(__dirname + "/public/pug/landing.html");
+    const auth_code = request.query.code;
+    const url = "https://accounts.spotify.com/api/token";
 
+    // your problem is here!!! you need to place all the body params in the url (like with ? and & you know)
 
+    const req_params = {
+        method: "POST",
+        body: {
+            grant_type: "authorization_code",
+            code: auth_code,
+            redirect_uri: redirect_uri
+        },
+        headers: {
+            "Authorization": "Basic " + my_client_id_64 + ":" + my_client_secret_64
+        }
+    };
+    const data = await fetch(url, req_params);
 
-
-
-
-
+    console.log(data);
+}
 
 
 
