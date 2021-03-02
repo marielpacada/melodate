bf1fca01dfed2d3a2ce9ea0b6f925e88482342c4
Mariel Pacada <49132861+marielpacada@users.noreply.github.com>
2021-03-02 00:29:59 -0800

    commented, next up home page html

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* Loading modules */
 global.fetch = require("node-fetch");
 const querystring = require('querystring');
 const express = require("express");
@@ -5,18 +6,22 @@ const cookieparser = require("cookie-parser");
 const request = require('request');
 const app = express();
 
+/* Importing authorization IDs */
 const { my_client_id } = require('./secrets/auth.js');
 const { my_client_secret } = require('./secrets/auth.js');
-const { resolve4 } = require("dns");
-const redirect_uri = "http://localhost:3000/callback";
+const redirect_uri = "http://localhost:3000/home";
 
+/* Registering middleware and settings */
 app.use(cookieparser());
 app.use(express.static("public"));
 app.set("view engine", "pug");
 app.set("query parser", "extended");
 app.set("views", __dirname + "/html");
 
-
+/**
+ * Login page. 
+ * Redirects user to Spotify authorization page, then redirects back to web app after logging in with Spotify account.
+ */
 app.get('/login', function (req, res) {
     var scopes = 'user-top-read playlist-modify-public user-follow-modify';
     res.redirect('https://accounts.spotify.com/authorize' + '?response_type=code' +
@@ -24,9 +29,12 @@ app.get('/login', function (req, res) {
         '&redirect_uri=' + encodeURIComponent(redirect_uri)) + "&state=state";
 });
 
-
-
-app.get('/callback', async function (req, res) {
+/**
+ * Home page.
+ * Handles all API requests for information on user profile (name, image, follower count, genre list, and id).
+ */
+app.get('/home', async function (req, res) {
+    // Gathering token via POST request to Spotify
     const auth_code = await req.query.code || null;
     var auth_options = {
         url: 'https://accounts.spotify.com/api/token',
@@ -40,106 +48,148 @@ app.get('/callback', async function (req, res) {
         }
     };
 
+    // Using access token (from previous POST request) to gather all artist profile data
     request.post(auth_options, function (error, response, body) {
-
         if (!error && response.statusCode === 200) {
             const access_token = body.access_token;
-            // getting user's top artists
             var top_artists_options = {
                 url: 'https://api.spotify.com/v1/me/top/artists?limit=50&time_range=short_term',
                 headers: { 'Authorization': 'Bearer ' + access_token },
                 json: true
             };
 
-            getUserTopArists(top_artists_options, access_token, function (artists) {
-                var artist_info = JSON.parse(JSON.stringify(artists));
-                res.render("landing", { artist_arr: artist_info });
+            // Getting all information needed for home (swiping) page
+            getAllArtistInfo(top_artists_options, access_token, function (artistRes) {
+                var artist_info = JSON.parse(JSON.stringify(artistRes));
+                // Sends artist info as a header (to be iterated in pug file)
+                res.render("landing", { artists: artist_info });
             });
-        } else {
+        } else { // Redirects on error
             res.redirect('/#' + querystring.stringify({ error: 'invalid_token' }));
         }
     });
 });
 
-function getUserTopArists(options, token, callback) {
-    request.get(options, async function (error, response, body) {
-        var user_artists = body.items;
-        var related_artists = await getRelatedArtists(user_artists, token);
-        var initial_pool = getInitialArtistPool(related_artists);
-        var final_pool = await getFinalArtistPool(initial_pool, token);
-        var info = JSON.parse(JSON.stringify(await getArtistInfo(final_pool.artists)));
-        return callback(info);
-    });
-}
+/* Listening on PORT 3000 */
+app.listen(3000);
 
-async function getRelatedArtists(userData, token) {
-    var related_artists = {};
-    for (artist in userData) {
-        var artist_id = userData[artist].id;
-        var url = "https://api.spotify.com/v1/artists/" + artist_id + "/related-artists";
-        var related_artists_options = {
+/**
+ * Helper function (with inner private functions) to get all artists' information
+ * @param {*} options object containing GET request parameters
+ * @param {*} token access token for API calls
+ * @param {*} callback function to access response data
+ */
+function getAllArtistInfo(options, token, callback) {
+
+    /* Inner private functions _______________________________________________________________________________ */
+
+    /**
+     * Sends GET request to Spotify for related artists of _each_ artist
+     * @param {*} userData user's top artists in the last four weeks (short-term)
+     * @param {*} token access token for API calls
+     * Returns map of user's top artist's index to that artist's related artists
+     * -- Example: { '0' : [array of artist objects], '1' : [array of artist objects], ... } 
+     */
+    async function _getRelatedArtists(userData, token) {
+        var related_artists = {};
+        for (artist in userData) {
+            var artist_id = userData[artist].id;
+            var url = "https://api.spotify.com/v1/artists/" + artist_id + "/related-artists";
+            var related_artists_options = {
+                method: "GET",
+                headers: { 'Authorization': 'Bearer ' + token },
+            };
+
+            const result = await fetch(url, related_artists_options);
+            const item = JSON.parse(JSON.stringify(await result.json()));
+            related_artists[JSON.parse(JSON.stringify(artist))] = item;
+        }
+        return related_artists;
+    }
+
+    /**
+     * Narrows down list of related artists based on popularity criteria
+     * @param {*} data the related artist map from previous function
+     * Returns array of Spotify Artist objects
+     */
+    function _getInitialArtistPool(data) {
+        var initial_pool = [];
+        for (artist in data) {
+            for (const x of Array(20).keys()) { // Each artist has 20 related artists
+                var id = JSON.parse(JSON.stringify(data[artist].artists[x].id));
+                // Avoids duplicates (artist overlap) and filters through artists who are fairly popular
+                if (!initial_pool.includes(id) &&
+                    data[artist].artists[x].popularity < 70 &&
+                    data[artist].artists[x].popularity > 60) {
+                    initial_pool.push(id);
+                }
+            }
+        }
+        return initial_pool;
+    }
+
+    /**
+     * Chooses 50 artists and produces a query parameter from their artist ids
+     * @param {*} arr array of artists
+     * Returns string to be passed as a query paramter to an API call
+     */
+    function _getRandomArtists(arr) {
+        var query = "";
+        for (const x of Array(50).keys()) {
+            var artistID = arr[Math.floor(Math.random() * arr.length)];
+            query = query + artistID + "%2C"; // Comma in URI
+        }
+        return query.slice(0, query.length - 3);
+    }
+
+    /**
+     * Chooses 50 artists from initial pool and sends GET request to Spotify for their artist info
+     * @param {*} initial array of artists from initial pool
+     * @param {*} token access token for API call
+     * Returns object in JSON format containing artists and their info
+     */
+    async function _getFinalArtistPool(initial, token) {
+        // Gets multiple artists with this specific GET request (max 50 artists)
+        var artist_query = _getRandomArtists(initial);
+        var artist_url = "https://api.spotify.com/v1/artists?ids=" + artist_query;
+        var artist_options = {
             method: "GET",
             headers: { 'Authorization': 'Bearer ' + token },
         };
 
-        const result = await fetch(url, related_artists_options);
-        const item = JSON.parse(JSON.stringify(await result.json()));
-        related_artists[JSON.parse(JSON.stringify(artist))] = item;
+        const artist_pool = await fetch(artist_url, artist_options);
+        const final_pool = JSON.parse(JSON.stringify(await artist_pool.json()));
+        return final_pool;
     }
-    return related_artists;
-}
 
-function getInitialArtistPool(data) {
-    var initial_pool = [];
-    for (artist in data) {
-        for (const x of Array(20).keys()) {
-            var id = JSON.parse(JSON.stringify(data[artist].artists[x].id));
-
-            if (!initial_pool.includes(id) &&
-                data[artist].artists[x].popularity < 70 &&
-                data[artist].artists[x].popularity > 60) {
-                initial_pool.push(id);
-            }
+    /**
+     * Iterates through artists and parses out necessary information
+     * @param {*} data final array of 50 artists to be swiped on
+     * Returns array of artists mapped to their info that the program needs
+     */
+    async function _getEachArtistInfo(data) {
+        var artists = [];
+        for (const x of Array(50).keys()) {
+            var item = {};
+            item["name"] = JSON.parse(JSON.stringify(data[x].name));
+            item["image"] = JSON.parse(JSON.stringify(data[x].images[0].url));
+            item["follow"] = JSON.parse(JSON.stringify(data[x].followers.total));
+            item["genre"] = JSON.parse(JSON.stringify(data[x].genres));
+            item["id"] = JSON.parse(JSON.stringify(data[x].id));
+            artists.push(item);
         }
+        return artists;
     }
-    return initial_pool;
+
+    /* _______________________________________________________________________________________________________ */
+
+    /* Returns callback function in order to access final artist array in "/home" GET request */
+    request.get(options, async function (error, response, body) {
+        var user_artists = body.items;
+        var related_artists = await _getRelatedArtists(user_artists, token);
+        var initial_pool = _getInitialArtistPool(related_artists);
+        var final_pool = await _getFinalArtistPool(initial_pool, token);
+        var info = JSON.parse(JSON.stringify(await _getEachArtistInfo(final_pool.artists)));
+        return callback(info);
+    });
 }
-
-async function getFinalArtistPool(initial, token) {
-    var artist_query = getRandomArtists(initial);
-    var artist_url = "https://api.spotify.com/v1/artists?ids=" + artist_query;
-    var artist_options = {
-        method: "GET",
-        headers: { 'Authorization': 'Bearer ' + token },
-    };
-
-    const artist_pool = await fetch(artist_url, artist_options);
-    const final_pool = JSON.parse(JSON.stringify(await artist_pool.json()));
-    return final_pool;
-}
-
-// lessen the shit we have to render
-// for each artist, we need image, name, followers, and genres
-async function getArtistInfo(data) {
-    var artists = [];
-    for (const x of Array(50).keys()) {
-        var item = {};
-        item["name"] = JSON.parse(JSON.stringify(data[x].name));
-        item["image"] = JSON.parse(JSON.stringify(data[x].images[0].url));
-        item["follow"] = JSON.parse(JSON.stringify(data[x].followers.total));
-        item["genre"] = JSON.parse(JSON.stringify(data[x].genres));
-        artists.push(item);
-    }
-    return artists;
-}
-
-function getRandomArtists(arr) {
-    var query = "";
-    for (const x of Array(50).keys()) {
-        var artistID = arr[Math.floor(Math.random() * arr.length)];
-        query = query + artistID + "%2C";
-    }
-    return query.slice(0, query.length - 3);
-}
-
-app.listen(3000);
